045fa6765348ba8bc06b8bae97a6f2229201a391
zwaclaw <zwaclaw@gmail.com>
2017-02-28 16:00:12 +0200

    Initial commit

---
+++ src/TestOne.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author zwaclaw
+ * @version 1.0
+ */
+
+public class TestOne{
+    public static void main(String[] args) throws UnsupportedEncodingException,
+            NoSuchAlgorithmException {
+        byte[] theTextToDigestAsBytes=
+                "Mano gyvenimas yra labai geras ..."
+                        .getBytes("8859_1");
+        MessageDigest md= MessageDigest.getInstance("MD5");
+        md.update(theTextToDigestAsBytes);
+        byte[] digest= md.digest();
+
+        for(byte b: digest){
+            System.out.printf("%02X", b & 0xff);
+        }
+        System.out.println();
+    }
+}
